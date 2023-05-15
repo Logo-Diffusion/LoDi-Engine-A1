10128a3588bb0a21b4a34c0c86573f80f98287c2
Atrin Hojjat <atrin.hojjat@gmail.com>
2023-05-15 23:13:38 +0330

    Create output directories

--- loadData.sh
+++ loadData.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-wget --header="Authorization: Bearer $HUGGINGFACE_TOKEN" https://huggingface.co/Artifice-ArtHouse/LoDi_Sports/resolve/main/LoDi_Sports_v1_sd15_noema_fp16.safetensors -P /app/models/Stable-diffusion/  --content-disposition
+mkdir -p /app/extensions/sd-webui-controlnet/models/
+mkdir -p /app/models/Stable-diffusion/
 
+wget --header="Authorization: Bearer $HUGGINGFACE_TOKEN" https://huggingface.co/Artifice-ArtHouse/LoDi_Sports/resolve/main/LoDi_Sports_v1_sd15_noema_fp16.safetensors -P /app/models/Stable-diffusion/  --content-disposition
 
 wget  https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/control_v11p_sd15_canny.pth -P /app/extensions/sd-webui-controlnet/models/  --content-disposition
 wget  https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/control_v11p_sd15_canny.yaml -P /app/extensions/sd-webui-controlnet/models/  --content-disposition
