3d7ee648c12c387cc1b358752e97c2802ccc27fa
Atrin Hojjat <atrin.hojjat@gmail.com>
2023-05-16 03:18:41 +0330

    Print hugging face token

--- loadData.sh
+++ loadData.sh
@@ -3,6 +3,8 @@
 mkdir -p /app/extensions/sd-webui-controlnet/models/
 mkdir -p /app/models/Stable-diffusion/
 
+echo "Token: "$HUGGINGFACE_TOKEN
+
 wget --header="Authorization: Bearer $HUGGINGFACE_TOKEN" https://huggingface.co/Artifice-ArtHouse/LoDi_Sports/resolve/main/LoDi_Sports_v1_sd15_noema_fp16.safetensors -P /app/models/Stable-diffusion/  --content-disposition
 
 wget  https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/control_v11p_sd15_canny.pth -P /app/extensions/sd-webui-controlnet/models/  --content-disposition
