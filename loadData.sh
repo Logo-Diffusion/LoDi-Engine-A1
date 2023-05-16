92c573aa1538515fc79e52f486e85a3ebdddbbb9
Atrin Hojjat <atrin.hojjat@gmail.com>
2023-05-16 03:35:05 +0330

    Check if files exist before downloading

--- loadData.sh
+++ loadData.sh
@@ -5,7 +5,9 @@ mkdir -p /app/models/Stable-diffusion/
 
 echo "Token: "$HUGGINGFACE_TOKEN
 
-wget --header="Authorization: Bearer $HUGGINGFACE_TOKEN" https://huggingface.co/Artifice-ArtHouse/LoDi_Sports/resolve/main/LoDi_Sports_v1_sd15_noema_fp16.safetensors -P /app/models/Stable-diffusion/  --content-disposition
+if [ ! -f /app/models/Stable-diffusion/LoDi_Sports_v1_sd15_noema_fp16.safetensors ]; then
+  wget --header="Authorization: Bearer $HUGGINGFACE_TOKEN" https://huggingface.co/Artifice-ArtHouse/LoDi_Sports/resolve/main/LoDi_Sports_v1_sd15_noema_fp16.safetensors -P /app/models/Stable-diffusion/  --content-disposition
+fi
 
 wget  https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/control_v11p_sd15_canny.pth -P /app/extensions/sd-webui-controlnet/models/  --content-disposition
 wget  https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/control_v11p_sd15_canny.yaml -P /app/extensions/sd-webui-controlnet/models/  --content-disposition
@@ -13,5 +15,10 @@ wget  https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/control_v11p_s
 wget  https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/control_v11p_sd15_scribble.pth -P /app/extensions/sd-webui-controlnet/models/  --content-disposition
 wget  https://huggingface.co/lllyasviel/ControlNet-v1-1/blob/main/control_v11p_sd15_scribble.yaml -P /app/extensions/sd-webui-controlnet/models/  --content-disposition
 
-wget --header="Authorization: Bearer $HUGGINGFACE_TOKEN" https://huggingface.co/Artifice-ArtHouse/3dlook.v1.0/resolve/main/3dmodel2_6624.safetensors -P /app/models/Stable-diffusion/  --content-disposition
-wget --header="Authorization: Bearer $HUGGINGFACE_TOKEN" https://huggingface.co/Artifice-ArtHouse/3dlook.v1.0/resolve/main/3dmodel2_6624.yaml -P /app/models/Stable-diffusion/  --content-disposition
+if [ ! -f /app/models/Stable-diffusion/3dmodel2_6624.safetensors ]; then
+  wget --header="Authorization: Bearer $HUGGINGFACE_TOKEN" https://huggingface.co/Artifice-ArtHouse/3dlook.v1.0/resolve/main/3dmodel2_6624.safetensors -P /app/models/Stable-diffusion/  --content-disposition
+fi
+
+if [ ! -f /app/models/Stable-diffusion/3dmodel2_6624.yaml ]; then
+  wget --header="Authorization: Bearer $HUGGINGFACE_TOKEN" https://huggingface.co/Artifice-ArtHouse/3dlook.v1.0/resolve/main/3dmodel2_6624.yaml -P /app/models/Stable-diffusion/  --content-disposition
+fi
